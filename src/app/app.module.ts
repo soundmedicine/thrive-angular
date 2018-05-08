cd53a134300e7a1f60c5c75a00343c9e4ec5845c
Roberto Santos <robot-o@hotmail.com>
2018-05-08 11:44:46 -0600

    add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { HttpClientModule } from '@angular/common/http'
 import { GrowthService } from './growth.service'
+import { RouterModule, Routes } from '@angular/router'
+import { AppComponent } from './app.component'
+import { AboutComponent } from './about/about.component'
+import { CompareComponent } from './compare/compare.component'
 
-import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { CompareComponent } from './compare/compare.component';
-
+const appRoutes: Routes = [
+  {path: '', redirectTo: '/about', pathMatch: 'full'},
+  {path: 'compare', component: CompareComponent},
+  {path: 'about', component: AboutComponent},
+  {path: '**', redirectTo: '/about', pathMatch: 'full'}
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,7 +21,8 @@ import { CompareComponent } from './compare/compare.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [GrowthService],
   bootstrap: [AppComponent]
