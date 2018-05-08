b8eba08610386755025ae03ea0db828bf05ae47a
Roberto Santos <robot-o@hotmail.com>
2018-05-08 10:00:03 -0600

    add materialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core'
 import { HttpClientModule } from '@angular/common/http'
 import { GrowthService } from './growth.service'
 
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
